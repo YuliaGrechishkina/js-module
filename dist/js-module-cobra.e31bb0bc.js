28e087075f244d46080318d6453b0881a2846d34
YuliaGrechishkina <yulia.grechishkina@gmail.com>
2020-10-30 13:49:48 +0200

    :art: edded homework_JS_2

--- dist/js-module-cobra.e31bb0bc.js
+++ dist/js-module-cobra.e31bb0bc.js
@@ -122,47 +122,47 @@ function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only")
 
 document.getElementById("SayYourName").onclick = function SayYourName() {
   var name = prompt("Ваше имя?", "");
-  alert('Привет,' + name);
+  alert("Привет, " + name);
 };
 
 document.getElementById("SayYourBthYear").onclick = function SayYourBthYear() {
   var today = 2020;
   var BthYear = prompt("Ваш год рождения?");
   var age = today - BthYear;
-  alert('Вам' + age);
+  alert("Вам " + age);
 };
 
 document.getElementById("SquareSide").onclick = function SquareSide() {
   var SquareSide = prompt("Сторона квадрата?", "");
   var perimeter = SquareSide * 4;
-  alert('Периметр квадрата' + perimeter);
+  alert("Периметр квадрата " + perimeter);
 };
 
 document.getElementById("CircleRadius").onclick = function CircleRadius() {
   var CircleRadius = prompt("Радиус окружности?", "");
-  var CircleArea = 3.14 * (CircleRadius * CircleRadius);
-  alert('Радиус окружности' + CircleArea);
+  var CircleArea = 3.1415926535 * (CircleRadius * CircleRadius);
+  alert("Радиус окружности " + CircleArea);
 };
 
 document.getElementById("Travel").onclick = function Travel() {
   var distance = prompt("Расстояние в км между двумя городами?", "");
   var hours = prompt("За сколько часов зотите добраться?", "");
   var speed = distance / hours;
-  alert('Если хотите успеть вовремя,едьте со скоростью' + distance + 'км в час');
+  alert("Если хотите успеть вовремя,едьте со скоростью " + distance + " км в час");
 };
 
 document.getElementById("CurrencyConverter").onclick = function CurrencyConverter() {
   var crossCourse = 0.85;
   var USD = prompt("Введите сумму в долл. США");
   var EURO = USD * crossCourse;
-  alert('За Ваши доллары Вы можете получить' + EURO + 'евро');
+  alert("За Ваши доллары Вы можете получить " + EURO + " евро");
 };
 
 document.getElementById("largeFlashDrive").onclick = function largeFlashDrive() {
   var largeFlashDrive = prompt("Объем Вашей флешкив Гб?", "");
   var fileSize = 820;
   var numberFiles = largeFlashDrive * 1000 / fileSize;
-  alert('На Вашу флешку поместится' + numberFiles + 'файлов размером в 820 Мб');
+  alert("На Вашу флешку поместится " + numberFiles + " файлов размером в 820 Мб");
 };
 
 document.getElementById("chocolate").onclick = function chocolate() {
@@ -172,9 +172,9 @@ document.getElementById("chocolate").onclick = function chocolate() {
   var change = money % price;
 
   if (amount > 1) {
-    alert('Вам хватает на' + amount + 'плитки шоколада');
+    alert("Вам хватает на " + amount + " плитки шоколада");
   } else {
-    alert("\u041A \u0441\u043E\u0436\u0430\u043B\u0435\u043D\u0438\u044E, \u0441 \u0442\u0430\u043A\u0438\u043C \u0444\u0438\u043D\u0430\u043D\u0441\u043E\u0432\u044B\u043C \u0441\u043E\u0441\u0435\u044F\u043D\u0438\u0435\u043C, \u0412\u044B \u043E\u0441\u0442\u0430\u043D\u0435\u0442\u0435\u0441\u044C \u0431\u0435\u0437 \u0441\u043B\u0430\u0434\u043A\u043E\u0433\u043E");
+    alert("К сожалению, с таким финансовым сосеянием, Вы останетесь без сладкого");
   }
 };
 
@@ -195,7 +195,176 @@ document.getElementById("deposit").onclick = function deposit() {
   var deposit = prompt("Пожалуйста,ведите сумму, которую планируете положить на депозит", "");
   var interestRate = 0.05;
   var accruedInterest = deposit * interestRate * 61 / 365.242;
-  alert("\u0421\u0443\u043C\u043C\u0430 \u043D\u0430\u0447\u0438\u0441\u043B\u0435\u043D\u043D\u044B\u0445 \u043F\u0440\u043E\u0446\u0435\u043D\u0442\u043E\u0432 \u0437\u0430 2 \u043C\u0435\u0441\u044F\u0446\u0430 \u0441\u043E\u0441\u0442\u0430\u0432\u0438\u0442" + accruedInterest);
+  alert("Сумма начисленных процентов за 2 месяца составит " + accruedInterest);
+}; //////////////////////////////////////////////////
+
+
+document.getElementById("age").onclick = function age() {
+  var age = prompt("Сколько Вам лет?", 0);
+
+  if (0 <= age && age < 2) {
+    alert("Вы младенец");
+  } else if (2 <= age && age < 12) {
+    alert("Вы ребенок");
+  } else if (2 <= age && age < 18) {
+    alert("Вы подросток");
+  } else if (18 <= age && age < 60) {
+    alert("Вы взрослый");
+  } else if (age >= 60) {
+    alert("Вы пенсионер");
+  }
+};
+
+document.getElementById("guessTheNumber").onclick = function guessTheNumber() {
+  var number = prompt("Введите число от 0  до 9", 0);
+
+  switch (number) {
+    case "0":
+      alert("спецсимвол, который расположен на этой клавише )");
+      break;
+
+    case "1":
+      alert("спецсимвол, который расположен на этой клавише !");
+      break;
+
+    case "2":
+      alert("спецсимвол, который расположен на этой клавише @");
+      break;
+
+    case "3":
+      alert("спецсимвол, который расположен на этой клавише #");
+      break;
+
+    case "4":
+      alert("спецсимвол, который расположен на этой клавише $");
+      break;
+
+    case "5":
+      alert("спецсимвол, который расположен на этой клавише %");
+      break;
+
+    case "6":
+      alert("спецсимвол, который расположен на этой клавише ^");
+      break;
+
+    case "7":
+      alert("спецсимвол, который расположен на этой клавише &");
+      break;
+
+    case "8":
+      alert("спецсимвол, который расположен на этой клавише *");
+      break;
+
+    case "9":
+      alert("спецсимвол, который расположен на этой клавише (");
+      break;
+  }
+};
+
+document.getElementById("threeDigitNumber").onclick = function threeDigitNumber() {
+  var threeDigitNumber = prompt("Введите трехзначное число", "");
+  var threeDigitNumber1 = parseInt(threeDigitNumber / 100);
+  var threeDigitNumber2 = parseInt(threeDigitNumber / 10) % 10;
+  var threeDigitNumber3 = threeDigitNumber % 10;
+  if (threeDigitNumber1 === threeDigitNumber2 || threeDigitNumber2 === threeDigitNumber3 || threeDigitNumber1 === threeDigitNumber3) alert("Найдено совпадение цифр");else alert("cовпадений не найдено");
+};
+
+document.getElementById("leapYear").onclick = function leapYear() {
+  var year = prompt("Введите год", "");
+
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 == 0) {
+        alert("Год  " + year + " не високосный");
+      } else alert("Год  " + year + " не високосный");
+    } else alert("Год  " + year + " високосный");
+  }
+
+  alert("Год  " + year + " не високосный");
+};
+
+document.getElementById("palindrome").onclick = function palindrome() {
+  var str = prompt("Введите число", "");
+  var j = 0,
+      le = str.length - 1,
+      pal = true;
+
+  while (j <= le - j) {
+    pal = pal && str.charAt(j) == str.charAt(le - j);
+    j++;
+  }
+
+  alert('Введённое число ' + str + (pal ? ' ' : ' не ') + 'является палиндромом');
+};
+
+document.getElementById("CurrenciesConverter").onclick = function CurrenciesConverter() {
+  var USD = prompt("Введите сумму в долл. США");
+  var currency = prompt("Выберите валюту конвертации: EUR - 1, UAN - 2 или AZN - 3");
+
+  switch (currency) {
+    case '1':
+      alert("За Ваши " + "USD" + " Вы получите " + USD * 0.86 + " EUR ");
+      break;
+
+    case '2':
+      alert("За Ваши " + "USD" + " Вы получите " + USD * 28.50 + " UAN ");
+      break;
+
+    case '3':
+      alert("За Ваши " + "USD" + " Вы получите " + USD * 1.70 + " AZN ");
+      break;
+  }
+};
+
+document.getElementById("discount").onclick = function discount() {
+  var purchaseAmount = prompt("Введите сумму покупки");
+
+  if (purchaseAmount >= 0 && purchaseAmount < 200) {
+    alert("Извините, на такую суму скидка не предусмотрена");
+  } else if (purchaseAmount >= 200 && purchaseAmount < 300) {
+    alert("Ваша скидка = 3%. " + " Сумма к оплате " + (purchaseAmount - purchaseAmount * 0.03));
+  } else if (purchaseAmount >= 300 && purchaseAmount < 500) {
+    alert("Ваша скидка = 5%. " + " Сумма к оплате " + (purchaseAmount - purchaseAmount * 0.05));
+  } else if (purchaseAmount >= 500) {
+    alert("Ваша скидка = 7%. " + " Сумма к оплате " + (purchaseAmount - purchaseAmount * 0.07));
+  }
+};
+
+document.getElementById("geometry").onclick = function geometry() {
+  var circumFerence = prompt("Введите длину окружности");
+  var squarePerimeter = prompt("Введите периметр квадрата");
+  var circleRadius = circumFerence / (2 * 3.1415926535);
+  var squareSide = squarePerimeter / 4;
+
+  if (circleRadius <= squareSide / 2) {
+    alert("Такая окружность поместится в указанный квадрат");
+  } else if (circleRadius > squareSide / 2) {
+    alert("Такая окружность  не поместится в указанный квадрат");
+  }
+};
+
+document.getElementById("interview").onclick = function interview() {
+  var firstQuestion = prompt("Куда на курортных пляжах просят не заплывать отдыхающих? 1 - За горизонт. 2 -  За буйки. 3 - В камыши.");
+  var secondQuestion = prompt("При падении чего принято загадывать желание? 1 - Температуры. 2 -  Курса гривны. 3 - Звезды.");
+  var thirdQuestion = prompt("Какое из этих животных чаще всего подвержено бреду? 1 - Сивая кобыла. 2 -  Лапчатый гусь. 3 - Дареный конь.");
+  var score = 0;
+
+  if (firstQuestion == 2) {
+    score += 2;
+  }
+
+  ;
+  if (secondQuestion == 3) score += 2;
+  if (thirdQuestion == 1) score += 2;
+  alert("Поздравляем, Вы набрали " + score + " баллов");
+};
+
+document.getElementById("сalendar").onclick = function сalendar() {
+  var date = prompt("Введите дату (день - месяц - год)");
+  date = date.split('-');
+  var oldDate = new Date(date[2], date[1] - 1, date[0]);
+  oldDate.setDate(oldDate.getDate() + 1);
+  alert(oldDate);
 };
 },{}],"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -225,7 +394,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55503" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60521" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 document.getElementById("SayYourName").onclick = function SayYourName() {
     const name = prompt("Ваше имя?", "");
-    alert('Привет,' + name);
+    alert("Привет, " + name);
 }
 
 document.getElementById("SayYourBthYear").onclick = function SayYourBthYear() {
     const today = 2020
     const BthYear = prompt("Ваш год рождения?");
     const age = today - BthYear;
-    alert('Вам' + age);
+    alert("Вам " + age);
 }
 
 document.getElementById("SquareSide").onclick = function SquareSide() {
     const SquareSide = prompt("Сторона квадрата?", "");
     const perimeter = SquareSide * 4;
-    alert('Периметр квадрата' + perimeter);
+    alert("Периметр квадрата " + perimeter);
 }
 
 document.getElementById("CircleRadius").onclick = function CircleRadius() {
     const CircleRadius = prompt("Радиус окружности?", "");
-    const CircleArea = 3.14 * (CircleRadius * CircleRadius);
-    alert('Радиус окружности' + CircleArea);
+    const CircleArea = 3.1415926535 * (CircleRadius * CircleRadius);
+    alert("Радиус окружности " + CircleArea);
 }
 
 document.getElementById("Travel").onclick = function Travel() {
     const distance = prompt("Расстояние в км между двумя городами?", "");
     const hours = prompt("За сколько часов зотите добраться?", "");
     const speed = distance / hours;
-    alert('Если хотите успеть вовремя,едьте со скоростью' + distance + 'км в час');
+    alert("Если хотите успеть вовремя,едьте со скоростью " + distance + " км в час");
 }
 
 document.getElementById("CurrencyConverter").onclick = function CurrencyConverter() {
     const crossCourse = 0.85;
     const USD = prompt("Введите сумму в долл. США");
     const EURO = USD * crossCourse;
-    alert('За Ваши доллары Вы можете получить' + EURO + 'евро');
+    alert("За Ваши доллары Вы можете получить " + EURO + " евро");
 }
 
 document.getElementById("largeFlashDrive").onclick = function largeFlashDrive() {
     const largeFlashDrive = prompt("Объем Вашей флешкив Гб?", "");
     const fileSize = 820;
     const numberFiles = (largeFlashDrive * 1000) / fileSize;
-    alert('На Вашу флешку поместится' + numberFiles + 'файлов размером в 820 Мб');
+    alert("На Вашу флешку поместится " + numberFiles + " файлов размером в 820 Мб");
 }
 
 document.getElementById("chocolate").onclick = function chocolate() {
@@ -49,9 +49,9 @@ document.getElementById("chocolate").onclick = function chocolate() {
     const amount = money / price;
     const change = money % price;
     if (amount > 1) {
-        alert('Вам хватает на' + amount + 'плитки шоколада')
+        alert("Вам хватает на " + amount + " плитки шоколада")
     } else {
-        alert(`К сожалению, с таким финансовым сосеянием, Вы останетесь без сладкого`);
+        alert("К сожалению, с таким финансовым сосеянием, Вы останетесь без сладкого");
     }
 }
 
@@ -71,6 +71,157 @@ document.getElementById("deposit").onclick = function deposit() {
     const deposit = prompt("Пожалуйста,ведите сумму, которую планируете положить на депозит", "");
     const interestRate = (0.05);
     let accruedInterest = ((deposit * interestRate * 61) / 365.242);
-    alert(`Сумма начисленных процентов за 2 месяца составит` + accruedInterest);
+    alert("Сумма начисленных процентов за 2 месяца составит " + accruedInterest);
 
+}
+
+//////////////////////////////////////////////////
+
+document.getElementById("age").onclick = function age() {
+    const age = prompt("Сколько Вам лет?", 0);
+    if (0 <= age && age < 2) {
+        alert("Вы младенец");
+    } else if (2 <= age && age < 12) {
+        alert("Вы ребенок");
+    } else if (2 <= age && age < 18) {
+        alert("Вы подросток");
+    } else if (18 <= age && age < 60) {
+        alert("Вы взрослый");
+    } else if (age >= 60) {
+        alert("Вы пенсионер");
+    }
+}
+
+document.getElementById("guessTheNumber").onclick = function guessTheNumber() {
+    const number = prompt("Введите число от 0  до 9", 0);
+    switch (number) {
+        case "0":
+            alert("спецсимвол, который расположен на этой клавише )");
+            break;
+        case "1":
+            alert("спецсимвол, который расположен на этой клавише !");
+            break;
+        case "2":
+            alert("спецсимвол, который расположен на этой клавише @");
+            break;
+        case "3":
+            alert("спецсимвол, который расположен на этой клавише #");
+            break;
+        case "4":
+            alert("спецсимвол, который расположен на этой клавише $");
+            break;
+        case "5":
+            alert("спецсимвол, который расположен на этой клавише %");
+            break;
+        case "6":
+            alert("спецсимвол, который расположен на этой клавише ^");
+            break;
+        case "7":
+            alert("спецсимвол, который расположен на этой клавише &");
+            break;
+        case "8":
+            alert("спецсимвол, который расположен на этой клавише *");
+            break;
+        case "9":
+            alert("спецсимвол, который расположен на этой клавише (");
+            break;
+    }
+}
+
+document.getElementById("threeDigitNumber").onclick = function threeDigitNumber() {
+    const threeDigitNumber = prompt("Введите трехзначное число", "");
+    const threeDigitNumber1 = parseInt(threeDigitNumber / 100);
+    const threeDigitNumber2 = parseInt(threeDigitNumber / 10) % 10;
+    const threeDigitNumber3 = (threeDigitNumber % 10);
+    if (threeDigitNumber1 === threeDigitNumber2 || threeDigitNumber2 === threeDigitNumber3 || threeDigitNumber1 === threeDigitNumber3)
+        alert("Найдено совпадение цифр");
+    else
+        alert("cовпадений не найдено");
+
+}
+
+document.getElementById("leapYear").onclick = function leapYear() {
+    const year = prompt("Введите год", "");
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                alert("Год  " + year + " не високосный");
+            } else
+                alert("Год  " + year + " не високосный");
+        } else
+            alert("Год  " + year + " високосный");
+    }
+    alert("Год  " + year + " не високосный");
+}
+
+document.getElementById("palindrome").onclick = function palindrome() {
+    const str = prompt("Введите число", "");
+    var j = 0,
+        le = str.length - 1,
+        pal = true;
+    while (j <= le - j) {
+        pal = pal && (str.charAt(j) == str.charAt(le - j));
+        j++
+    }
+    alert('Введённое число ' + str + (pal ? ' ' : ' не ') + 'является палиндромом');
+}
+
+document.getElementById("CurrenciesConverter").onclick = function CurrenciesConverter() {
+    const USD = prompt("Введите сумму в долл. США");
+    const currency = prompt("Выберите валюту конвертации: EUR - 1, UAN - 2 или AZN - 3");
+    switch (currency) {
+        case '1':
+            alert("За Ваши " + "USD" + " Вы получите " + USD * 0.86 + " EUR ");
+            break;
+        case '2':
+            alert("За Ваши " + "USD" + " Вы получите " + USD * 28.50 + " UAN ");
+            break;
+        case '3':
+            alert("За Ваши " + "USD" + " Вы получите " + USD * 1.70 + " AZN ");
+            break;
+    }
+}
+
+document.getElementById("discount").onclick = function discount() {
+    const purchaseAmount = prompt("Введите сумму покупки");
+    if (purchaseAmount >= 0 && purchaseAmount < 200) {
+        alert("Извините, на такую суму скидка не предусмотрена");
+    } else if (purchaseAmount >= 200 && purchaseAmount < 300) {
+        alert("Ваша скидка = 3%. " + " Сумма к оплате " + (purchaseAmount - purchaseAmount * 0.03));
+    } else if (purchaseAmount >= 300 && purchaseAmount < 500) {
+        alert("Ваша скидка = 5%. " + " Сумма к оплате " + (purchaseAmount - purchaseAmount * 0.05));
+    } else if (purchaseAmount >= 500) {
+        alert("Ваша скидка = 7%. " + " Сумма к оплате " + (purchaseAmount - purchaseAmount * 0.07));
+    }
+}
+
+document.getElementById("geometry").onclick = function geometry() {
+    const circumFerence = prompt("Введите длину окружности");
+    const squarePerimeter = prompt("Введите периметр квадрата");
+    const circleRadius = (circumFerence / (2 * 3.1415926535));
+    const squareSide = (squarePerimeter / 4);
+    if (circleRadius <= (squareSide / 2)) {
+        alert("Такая окружность поместится в указанный квадрат");
+    } else if (circleRadius > (squareSide / 2)) {
+        alert("Такая окружность  не поместится в указанный квадрат");
+    }
+}
+
+document.getElementById("interview").onclick = function interview() {
+    const firstQuestion = prompt("Куда на курортных пляжах просят не заплывать отдыхающих? 1 - За горизонт. 2 -  За буйки. 3 - В камыши.");
+    const secondQuestion = prompt("При падении чего принято загадывать желание? 1 - Температуры. 2 -  Курса гривны. 3 - Звезды.");
+    const thirdQuestion = prompt("Какое из этих животных чаще всего подвержено бреду? 1 - Сивая кобыла. 2 -  Лапчатый гусь. 3 - Дареный конь.");
+    let score = 0;
+    if (firstQuestion == 2) { score += 2 };
+    if (secondQuestion == 3) score += 2;
+    if (thirdQuestion == 1) score += 2;
+    alert("Поздравляем, Вы набрали " + score + " баллов");
+}
+
+document.getElementById("сalendar").onclick = function сalendar() {
+    let date = prompt("Введите дату (день - месяц - год)");
+    date = date.split('-');
+    let oldDate = new Date(date[2], date[1] - 1, date[0]);
+    oldDate.setDate(oldDate.getDate() + 1);
+    alert(oldDate);
 }
